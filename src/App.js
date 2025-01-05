8bc7d66cef5f1a40f7df8c2b1f3c5916a4276bf0
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2025-01-05 12:01:03 +0700

    push

--- src/App.js
+++ src/App.js
@@ -57,7 +57,29 @@ export class App extends Component {
     const { price, downPaymentPercentage, months, monthlyPayment, error, downPayment } = this.state;
 
     return (
-      <div className="container mt-5">
+      <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Twelfth navbar example">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">IMS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className='container'>
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
             <img
@@ -141,6 +163,7 @@ export class App extends Component {
               </div>
             )}
           </div>
+        </div>
         </div>
       </div>
     );
