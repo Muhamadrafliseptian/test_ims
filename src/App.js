255f8144a0965faac0b577b39174ce4524ecac01
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2025-01-05 11:54:15 +0700

    push

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ export class App extends Component {
     super(props);
     this.state = {
       price: 240000000,
-      downPaymentPercentage: '',
+      downPaymentPercentage: '10',
       months: '',
       monthlyPayment: '',
       error: '',
+      downPayment: ''
     };
   }
 
@@ -22,6 +23,11 @@ export class App extends Component {
     e.preventDefault();
     const { price, downPaymentPercentage, months } = this.state;
 
+    if (!months) {
+      this.setState({ error: 'Durasi cicilan harus diisi' });
+      return;
+    }
+
     if (months > 24) {
       this.setState({ error: 'Maksimal cicilan adalah 24 bulan' });
       return;
@@ -31,44 +37,46 @@ export class App extends Component {
       price,
       downPaymentPercentage,
       months,
-    }
+    };
 
     this.setState({ error: '' });
 
     try {
       const response = await axios.post('https://7040-2400-9800-6032-cdf-b873-5ad0-1e75-7266.ngrok-free.app/credit/calculate', postData);
 
-      const { monthlyPayment } = response.data;
-      this.setState({ monthlyPayment });
+      const { monthlyPayment, downPayment } = response.data.data;
+
+      // Menyimpan data ke state untuk ditampilkan
+      this.setState({ monthlyPayment, downPayment });
     } catch (error) {
       this.setState({ error: 'Terjadi kesalahan, coba lagi nanti.' });
     }
   };
 
   render() {
-    const { price, downPaymentPercentage, months, monthlyPayment, error } = this.state;
+    const { price, downPaymentPercentage, months, monthlyPayment, error, downPayment } = this.state;
 
     return (
       <div className="container mt-5">
-          <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div className="col-10 col-sm-8 col-lg-6">
-              <img
-                src="https://imgx.gridoto.com/crop/130x0:1279x670/700x465/photo/2021/11/02/toyota-avanza-veloz-at-2011jpeg-20211102111358.jpeg"  // Ganti dengan URL gambar mobil Anda
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <img
+              src="https://imgx.gridoto.com/crop/130x0:1279x670/700x465/photo/2021/11/02/toyota-avanza-veloz-at-2011jpeg-20211102111358.jpeg"  // Ganti dengan URL gambar mobil Anda
 
-                className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
-            </div>
-            <div className="col-lg-6">
-              <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Mobil Toyota Avanza</h1>
-              <p className="lead">
-                Toyota Avanza adalah salah satu mobil MPV terbaik di Indonesia yang menawarkan kenyamanan,
-                efisiensi bahan bakar, dan harga yang terjangkau. Dapatkan mobil impian Anda sekarang juga dengan
-                sistem kredit yang mudah dan cepat.
-              </p>
-            </div>
+              className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
           </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Mobil Toyota Avanza</h1>
+            <p className="lead">
+              Toyota Avanza adalah salah satu mobil MPV terbaik di Indonesia yang menawarkan kenyamanan,
+              efisiensi bahan bakar, dan harga yang terjangkau. Dapatkan mobil impian Anda sekarang juga dengan
+              sistem kredit yang mudah dan cepat.
+            </p>
+          </div>
+        </div>
 
 
-        <h2>Perhitungan Angsuran Kredit Mobil</h2>
+        <h2>Perhitungan Angsuran Kredit Mobil Avanza</h2>
 
         <form onSubmit={this.handleSubmit} className='mb-5'>
           <div className="mb-3">
@@ -105,7 +113,6 @@ export class App extends Component {
               value={months}
               onChange={this.handleChange}
               min="1"
-              max="24"
             />
             {error && <div className="text-danger">{error}</div>}
           </div>
@@ -113,14 +120,28 @@ export class App extends Component {
           <button type="submit" className="btn btn-primary">Hitung Angsuran</button>
         </form>
 
-        {monthlyPayment && (
-          <div className="mt-4">
-            <h4>Hasil Angsuran Per Bulan:</h4>
-            <div className="alert alert-info">
-              {monthlyPayment}
-            </div>
+        <div className='row'>
+          <div className='col-md-6'>
+            {downPayment && (
+              <div className="mt-4">
+                <h4>Uang Muka</h4>
+                <div className="alert alert-info">
+                  {downPayment}
+                </div>
+              </div>
+            )}
           </div>
-        )}
+          <div className='col-md-6'>
+            {monthlyPayment && (
+              <div className="mt-4">
+                <h4>Hasil Angsuran Per Bulan:</h4>
+                <div className="alert alert-info">
+                  {monthlyPayment}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
