dda26c6bc9a8f1af2e56e917bde1ae20aee6c858
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2025-01-05 11:18:55 +0700

    push

--- src/App.js
+++ src/App.js
@@ -50,8 +50,27 @@ export class App extends Component {
 
     return (
       <div className="container mt-5">
+          <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div className="col-10 col-sm-8 col-lg-6">
+              <img
+                src="https://imgx.gridoto.com/crop/130x0:1279x670/700x465/photo/2021/11/02/toyota-avanza-veloz-at-2011jpeg-20211102111358.jpeg"  // Ganti dengan URL gambar mobil Anda
+
+                className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+            </div>
+            <div className="col-lg-6">
+              <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Mobil Toyota Avanza</h1>
+              <p className="lead">
+                Toyota Avanza adalah salah satu mobil MPV terbaik di Indonesia yang menawarkan kenyamanan,
+                efisiensi bahan bakar, dan harga yang terjangkau. Dapatkan mobil impian Anda sekarang juga dengan
+                sistem kredit yang mudah dan cepat.
+              </p>
+            </div>
+          </div>
+
+
         <h2>Perhitungan Angsuran Kredit Mobil</h2>
-        <form onSubmit={this.handleSubmit}>
+
+        <form onSubmit={this.handleSubmit} className='mb-5'>
           <div className="mb-3">
             <label htmlFor="price" className="form-label">Harga Mobil</label>
             <input
@@ -63,7 +82,7 @@ export class App extends Component {
           </div>
 
           <div className="mb-3">
-            <label htmlFor="downPaymentPercentage" className="form-label">Down Payment (%)</label>
+            <label htmlFor="downPaymentPercentage" className="form-label">Uang Muka (%)</label>
             <select
               id="downPaymentPercentage"
               className="form-select"
